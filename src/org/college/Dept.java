ed4e94875d462e5a312ced161ea260c65634c046
VidyaSullia <vidyashreedhara@gmail.com>
2019-08-21 17:09:57 +0530

    ok

--- src/org/college/Dept.java
+++ src/org/college/Dept.java
@@ -4,5 +4,15 @@ public class Dept {
 	public void deptName() {
 		System.out.println("Dept Name: Information Technology");
 	}
+	
+	
+	
+	
+	
+	
+	public void name() {
+		System.out.println("vi");
+
+	}
 
 }
